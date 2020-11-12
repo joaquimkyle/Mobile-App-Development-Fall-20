198144d2e7f8ab3a19882ddce2e8d1c66181c3c9
joaquimkyle <joaquimkyle@gmail.com>
2020-11-11 21:12:19 -0500

    project update 11/11

---
+++ project/app/src/main/java/com/kjoaquim/todoro/ui/tasks/TasksViewModel.java
@@ -0,0 +1,19 @@
+package com.kjoaquim.todoro.ui.tasks;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class TasksViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public TasksViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is routines fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ project/app/src/main/java/com/kjoaquim/todoro/ui/work/WorkFragment.java
@@ -0,0 +1,35 @@
+package com.kjoaquim.todoro.ui.work;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.kjoaquim.todoro.R;
+
+public class WorkFragment extends Fragment {
+
+    private WorkViewModel workViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        workViewModel =
+                new ViewModelProvider(this).get(WorkViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_work, container, false);
+        final TextView textView = root.findViewById(R.id.text_work);
+        workViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
